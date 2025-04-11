a8c2838e5cfa7d1ca41857c4aaa61030e1173e6b
GeovanniMarrocos <geovanni.marrocos@gmail.com>
2025-04-11 00:07:23 -0300

    upload img files

---
+++ app/file/imageToText.php
@@ -0,0 +1,25 @@
+<?php 
+
+namespace App\file;
+
+class imageToText
+{
+    /**
+     * Recurso de imagem (GB)
+     * @var resource
+     */
+    private $image;
+
+    /**
+     * Largura original da imagem 
+     * @var integer
+     */
+    private $width;
+
+     /**
+     * Largura original da imagem 
+     * @var integer
+     */
+    private $height;
+
+}
